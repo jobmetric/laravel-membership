1a1f8f8575c8108272296a6d28b4cf22f83448ef
majid mohammadian <majeedmohammadian@gmail.com>
2024-04-04 19:55:55 +0330

    add language file

---
+++ lang/en/base.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Membership Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Membership for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    'validation' => [
+        'errors' => 'Validation errors occurred.',
+    ],
+
+    'messages' => [
+        'created' => 'The membership was created successfully.',
+        'updated' => 'The membership was updated successfully.',
+        'deleted' => 'The membership was deleted successfully.',
+    ],
+
+];
