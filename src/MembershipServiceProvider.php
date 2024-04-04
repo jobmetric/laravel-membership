b0bac2c1ecec738a8e833bf1c192c4f2de2b460f
majid mohammadian <majeedmohammadian@gmail.com>
2024-04-04 20:42:01 +0330

    add membership service provider

---
+++ src/MembershipServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace JobMetric\Membership;
+
+use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
+use JobMetric\PackageCore\Exceptions\RegisterClassTypeNotFoundException;
+use JobMetric\PackageCore\PackageCore;
+use JobMetric\PackageCore\PackageCoreServiceProvider;
+
+class MembershipServiceProvider extends PackageCoreServiceProvider
+{
+    /**
+     * @param PackageCore $package
+     *
+     * @return void
+     * @throws MigrationFolderNotFoundException
+     * @throws RegisterClassTypeNotFoundException
+     */
+    public function configuration(PackageCore $package): void
+    {
+        $package->name('laravel-membership')
+            ->hasConfig()
+            ->hasMigration()
+            ->hasTranslation()
+            ->registerClass('Membership', \JobMetric\Membership\Membership::class);
+    }
+}
