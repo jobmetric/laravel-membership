d356bd54e40bccfa8833edd327a7a8eeb4f0d87a
majid mohammadian <majeedmohammadian@gmail.com>
2024-07-20 21:02:59 +0330

    change model

--- src/Models/Member.php
+++ src/Models/Member.php
@@ -4,27 +4,26 @@ namespace JobMetric\Membership\Models;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
 /**
  * JobMetric\Membership\Models\Member
  *
- * @property int id
- * @property string user_id
+ * @property string personable_type
+ * @property int personable_id
  * @property string memberable_type
- * @property string memberable_id
+ * @property int memberable_id
  * @property string collection
- * @property string deleted_at
  * @property string created_at
- * @property string updated_at
  */
-class Member extends Model
+class Member extends Pivot
 {
     use HasFactory;
 
     protected $fillable = [
-        'user_id',
+        'personable_type',
+        'personable_id',
         'memberable_type',
         'memberable_id',
         'collection'
@@ -36,7 +35,8 @@ class Member extends Model
      * @var array<string, string>
      */
     protected $casts = [
-        'user_id' => 'integer',
+        'personable_type' => 'string',
+        'personable_id' => 'integer',
         'memberable_type' => 'string',
         'memberable_id' => 'integer',
         'collection' => 'string'
@@ -48,25 +48,35 @@ class Member extends Model
     }
 
     /**
-     * Set the scope of a query so that it only determines
+     * Personable relationship method.
+     *
+     * @return MorphTo
+     */
+    public function personable(): MorphTo
+    {
+        return $this->morphTo(__FUNCTION__, 'personable_type', 'personable_id');
+    }
+
+    /**
+     * Memberable relationship method.
+     *
+     * @return MorphTo
+     */
+    public function memberable(): MorphTo
+    {
+        return $this->morphTo(__FUNCTION__, 'memberable_type', 'memberable_id');
+    }
+
+    /**
+     * Scope a query to only include categories of a given collection.
      *
      * @param Builder $query
      * @param string $collection
      *
      * @return Builder
      */
-    public function scopeOfUser(Builder $query, string $collection): Builder
+    public function scopeOfCollection(Builder $query, string $collection): Builder
     {
         return $query->where('collection', $collection);
-    }
-
-    /**
-     * Memberable relationship method to get the model that the member belongs to.
-     *
-     * @return MorphTo
-     */
-    public function memberable(): MorphTo
-    {
-        return $this->morphTo();
     }
 }
